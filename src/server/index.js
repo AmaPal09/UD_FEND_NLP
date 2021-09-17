84b719cd3a2248a2e20899d4658c533806426b11
Amarja Palekar <amarjapalekar09@gmail.com>
2021-09-16 17:06:31 -0700

    Chng: Set-up index file on the server side.

---
+++ src/server/index.js
@@ -0,0 +1,43 @@
+//Server side JS code
+
+/*
+* ALL REQUIRED PACKAGES
+*/
+//web framework - express
+const express = require('express');
+//Cross-Origin Resource Sharing (CORS)
+const cors = require('cors');
+//Body parser to parse JSON
+const bodyParser = require('body-parser');
+
+// Start and instance of the app
+const app = express();
+
+//Configure express to use body-parser as middle-ware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// User cors for cross origin allowance. Use cors middleware
+app.use(cors());
+
+/*
+* SERVER SET-UP
+*/
+// Set a port to run the server on
+const port = 3000;
+
+// Serve static files from website folder and connect them with the server
+app.use(express.static('../client'));
+
+
+// Keep the server running and listen for activity
+app.listen(port, ()=>{
+	console.log(`Server running on port: ${port}`);
+});
+
+
+//Launch the homepage
+app.get('/', (req,res) => {
+	console.log("Get request");
+	res.sendFile('views/index.html');
+});
