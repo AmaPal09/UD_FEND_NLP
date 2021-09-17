a389efee871814fbcb1cfd3f8e51cc23f540ec07
Amarja Palekar <amarjapalekar09@gmail.com>
2021-09-17 12:07:20 -0700

    CHNG: Display index html files at local host 3000.
    Add content to index.html file
    Add route '/'
    Send index.html file to client when '/' route is requested
    Install express, body-parser, cors to run the express server

--- src/server/index.js
+++ src/server/index.js
@@ -27,7 +27,7 @@ app.use(cors());
 const port = 3000;
 
 // Serve static files from website folder and connect them with the server
-app.use(express.static('../client'));
+app.use(express.static('src/client'));
 
 
 // Keep the server running and listen for activity
@@ -39,5 +39,5 @@ app.listen(port, ()=>{
 //Launch the homepage
 app.get('/', (req,res) => {
 	console.log("Get request");
-	res.sendFile('views/index.html');
+	res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
 });
