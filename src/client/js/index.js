c599d6b6ab2210cf5f0a33effeff4a8b12e4ee62
Amarja Palekar <amarjapalekar09@gmail.com>
2021-09-21 11:58:31 -0700

    DOC: Remove commented code lines

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -71,30 +71,6 @@ function isValidURL(inputURL) {
 	return pattern.test(inputURL);
 }
 
-// var testCase1 = "http://en.wikipedia.org/wiki/Procter_&_Gamble";
-// console.log(isValidURL(testCase1)); // return true
-
-// var testCase2 = "http://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&docid=nIv5rk2GyP3hXM&tbnid=isiOkMe3nCtexM:&ved=0CAUQjRw&url=http%3A%2F%2Fanimalcrossing.wikia.com%2Fwiki%2FLion&ei=ygZXU_2fGKbMsQTf4YLgAQ&bvm=bv.65177938,d.aWc&psig=AFQjCNEpBfKnal9kU7Zu4n7RnEt2nerN4g&ust=1398298682009707";
-// console.log(isValidURL(testCase2)); // return true
-
-// var testCase3 = "https://sdfasd";
-// console.log(isValidURL(testCase3)); // return false
-
-// var testCase4 = "dfdsfdsfdfdsfsdfs";
-// console.log(isValidURL(testCase4)); // return false
-
-// var testCase5 = "magnet:?xt=urn:btih:123";
-// console.log(isValidURL(testCase5)); // return false
-
-// var testCase6 = "https://stackoverflow.com/";
-// console.log(isValidURL(testCase6)); // return true
-
-// var testCase7 = "https://w";
-// console.log(isValidURL(testCase7)); // return false
-
-// var testCase8 = "https://sdfasdp.ppppppppppp";
-// console.log(isValidURL(testCase8)); // return false
-
 
 /*
 event listener
