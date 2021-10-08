95bb42381b3a02bdb4968dc35fe43e763ec5fc90
Amarja Palekar <amarjapalekar09@gmail.com>
2021-10-08 08:46:36 -0700

    FEAT: Add Clean Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: './src/client/index.js',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
 	module: {
 		rules: [
 		//JavaScripts
@@ -18,6 +23,7 @@ module.exports = {
 	new HtmlWebPackPlugin({
 		template: './src/client/views/index.html',
 		filename: './index.html'
-	})
+	}),
+	new CleanWebpackPlugin()
 	]
 };
