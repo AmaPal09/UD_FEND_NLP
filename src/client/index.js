9a460ad209abaa115f8758d7ad45b2a8c682131d
Amarja Palekar <amarjapalekar09@gmail.com>
2021-10-08 09:46:16 -0700

    FEAT: Add color variables and import them to home page

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import { isValidURL } from './js/validateURL'
 import { submitForm } from './js/index.js'
 import { sendURLToServer } from './js/index.js'
 import './styles/resets.scss'
+import './styles/main.scss'
 
 alert("I exist");
 console.log("New index file used as entry point");
