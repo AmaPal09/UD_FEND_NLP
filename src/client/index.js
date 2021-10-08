28ff9b32a36e04040935882c7b3d601f6edeaae3
Amarja Palekar <amarjapalekar09@gmail.com>
2021-10-08 11:08:30 -0700

    CHNG: Add fonts to the style sheets

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import { isValidURL } from './js/validateURL'
 import { submitForm } from './js/index.js'
 import { sendURLToServer } from './js/index.js'
-import './styles/resets.scss'
+// import './styles/resets.scss'
 import './styles/main.scss'
 
 alert("I exist");
