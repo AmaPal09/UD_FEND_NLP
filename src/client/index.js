f1774e37b2ae9a70fc0234c2eac24c9a3e0ae260
Amarja Palekar <amarjapalekar09@gmail.com>
2021-10-13 11:36:33 -0700

    CHNG: Remove alert statements

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,11 @@
-import { isValidURL } from './js/validateURL'
-import { submitForm } from './js/index.js'
-import { sendURLToServer } from './js/index.js'
 // import './styles/resets.scss'
 import './styles/main.scss'
 
-alert("I exist");
+import { isValidURL } from './js/validateURL'
+import { submitForm } from './js/index.js'
+import { sendURLToServer } from './js/index.js'
+
+
+// alert("I exist");
 console.log("New index file used as entry point");
-alert("Hi there!");
+// alert("Hi there!");
