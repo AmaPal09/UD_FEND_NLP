12ac4498ecdea5e9f7c6404167a7255989c831c1
Amarja Palekar <amarjapalekar09@gmail.com>
2021-10-13 17:02:36 -0700

    DOC: Add comments with function descriptions, align comments with commands where possible and 		remove commented code

--- src/server/index.js
+++ src/server/index.js
@@ -2,26 +2,19 @@
 /*
 * ALL REQUIRED PACKAGES
 */
-//web framework - express
-const express = require('express');
-//Cross-Origin Resource Sharing (CORS)
-const cors = require('cors');
-//Body parser to parse JSON
-const bodyParser = require('body-parser');
-//Environment variables
-const dotenv = require('dotenv');
-//fetch command to access API
-const fetch = require('node-fetch');
+const express = require('express'); //web framework - express
+const cors = require('cors'); //Cross-Origin Resource Sharing (CORS)
+const bodyParser = require('body-parser'); //Body parser to parse JSON
+const dotenv = require('dotenv'); //Environment variables
+const fetch = require('node-fetch'); //fetch command to access API
 
-//configure env variables
-dotenv.config();
+dotenv.config(); //configure env variables
 // GLOBAL VARIABLES
 const SENTIMENT_API = 'https://api.meaningcloud.com/sentiment-2.1';
 const SENTIMENT_API_KEY = process.env.SENTIMENT_API_KEY;
 
 // Start and instance of the app
 const app = express();
-
 
 //Configure express to use body-parser as middle-ware
 app.use(bodyParser.urlencoded({extended: false}));
@@ -40,40 +33,51 @@ const port = 3000;
 // app.use(express.static('src/client'));
 app.use(express.static('dist'));
 
-
 // Keep the server running and listen for activity
 app.listen(port, ()=>{
 	console.log(`Server running on port: ${port}`);
 });
 
 
-//Launch the homepage
+/*
+* GET METHOD
+* @description: Launch the homepage
+* @param req: request with details of method and information about data requested
+* @param res: response sent with details of status and data
+*/
 app.get('/', (req,res) => {
 	console.log("Get request");
 	// res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
 	res.sendFile('dist/index.html');
 });
 
-// Process the form
+
+/*
+* POST METHOD
+* @description: Process user input form to obtain sentiment analysis for it and posts analysis 					for requesting route
+* @param req: request with details of method and information about data requested
+* @param res: response sent with details of status and data
+*/
 app.post('/artProcess', async(req, res) => {
 	console.log("req.body from client ===", req.body);
-	// const articleURL = req.body;
-	// const analysisResult = "You entered this " + articleURL + "URL";
-	// console.log(analysisResult);
 	const reqData = req.body;
-	console.log('url', reqData);
 
+	// Call fetchAnalysis function to perform the fetch request
 	const analysisResponse = await fetchAnalysis(reqData.arturl)
 	try {
 		sendResponse(analysisResponse, res);
 	}catch(error) {
 		console.log('error', error);
 	}
-
-
 });
 
 
+/*
+* sendResponse FUNCTION
+* @description: sends analysis response to client
+* @param {json} analysisResponse: sentiment analysis for provided url in json format
+* @param res: response sent with details of status and data
+*/
 function sendResponse(analysisResponse, res) {
 	console.log("sendResponse function");
 	// console.log("analysis Response: ", analysisResponse);
@@ -85,24 +89,17 @@ function sendResponse(analysisResponse, res) {
 }
 
 
+/*
+* fetchAnalysis FUNCTION
+* @description: Makes a fetch request to sentiment analysis API
+* @param {string} inurl: url for which sentiment analysis is requested
+* @return {json} newData: sentiment analysis response received from API
+*/
 const fetchAnalysis = async (inurl = '') => {
 	console.log("enter fetchAnalysis");
 	console.log(inurl);
-	const formData = {
-		// key: KEY,
-		url: inurl,
-		lang: 'auto'
-	}
 
-	const requestOptions = {
-		method: 'POST',
-		body: formData,
-		redirect: 'follow'
-	};
-
-	// const response = await fetch(API, requestOptions)
-	// const response = await fetch(API, {method: 'POST', body: formData})
-	console.log(`${SENTIMENT_API}?key=${SENTIMENT_API_KEY}&url=${inurl}&lang=auto`);
+	// console.log(`${SENTIMENT_API}?key=${SENTIMENT_API_KEY}&url=${inurl}&lang=auto`);
 	const response = await fetch( `${SENTIMENT_API}?key=${SENTIMENT_API_KEY}&url=${inurl}&lang=auto`, {method: 'POST'})
 
 	try {
