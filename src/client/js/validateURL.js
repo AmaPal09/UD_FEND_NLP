9c0a6c8880fbc7aac557bd0aedaab00a01de58e8
Amarja Palekar <amarjapalekar09@gmail.com>
2021-10-22 12:36:34 -0700

    DOC: Comment log statements

--- src/client/js/validateURL.js
+++ src/client/js/validateURL.js
@@ -7,7 +7,7 @@
 * @returns {Boolean}: true if it an URL, false if it is not
 */
 function isValidURL(inputURL) {
-	console.log("Validate URL: ", inputURL);
+	// console.log("Validate URL: ", inputURL);
  	const pattern = new RegExp('^((ft|htt)ps?:\\/\\/)?'+ // protocol
 	'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
 	'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
