e407add4d926233c33e28295fddf1570477c717a
Amarja Palekar <amarjapalekar09@gmail.com>
2021-10-28 17:28:47 -0700

    DOC: Remove Commented code

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,3 @@
-// import style sheets
-// import './styles/resets.scss'
 import './styles/main.scss'
 
 // import js scripts
@@ -9,9 +7,7 @@ import { sendURLToServer } from './js/index.js'
 export{printResult} from './js/printResults.js'
 export{createTableRow} from './js/printResults.js'
 
-// alert("I exist");
 console.log("New index file used as entry point");
-// alert("Hi there!");
 
 export {
 	isValidURL,
